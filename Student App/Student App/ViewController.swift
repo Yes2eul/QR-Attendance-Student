0cbcebeee7251ba987f01f7384ee603b402ca5f3
YeseulShin0 <Yeseul.Shin-1@student.uts.edu.au>
2023-05-19 18:03:00 +1000

    Create Project

---
+++ Student App/Student App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student App
+//
+//  Created by Yeseul Shin on 17/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
