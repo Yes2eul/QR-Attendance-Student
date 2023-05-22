4ff0b61a2b090cc7c159d716465863281aa72317
YeseulShin0 <Yeseul.Shin-1@student.uts.edu.au>
2023-05-22 15:17:45 +1000

    Create MVC

---
+++ Student App/Student App/Control/QRScanViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QRScanViewController.swift
+//  Student App
+//
+//  Created by Yeseul Shin on 22/5/2023.
+//
+
+import Foundation
+import UIKit
+
+class QRScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Student App/Student App/Control/StudentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StudentViewController.swift
+//  Student App
+//
+//  Created by Yeseul Shin on 22/5/2023.
+//
+
+import Foundation
+import UIKit
+
+class StudentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
