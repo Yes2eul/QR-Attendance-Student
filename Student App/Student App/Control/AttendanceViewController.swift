3af11bd9761adb7da05ea29b67c9d6695ed86ab2
YeseulShin0 <Yeseul.Shin-1@student.uts.edu.au>
2023-05-22 14:35:22 +1000

    Create controller files

---
+++ Student App/Student App/Control/AttendanceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AttendanceViewController.swift
+//  Student App
+//
+//  Created by Yeseul Shin on 22/5/2023.
+//
+
+import Foundation
+import UIKit
+
+class AttendanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
